e0ec4e8ed0de5888033989339cce7467936cd5a6
Nicolas Dubois <nicolas.c.dubois@gmail.com>
2023-03-15 23:32:05 +0100

    Remove superfluous `object`
    I've used the tool pyupgrade[^1] to perform this cleanup, using the
    following command:
    
    ```console
    $ find examples fastapi_hypermodel tests -name "*.py" \
        -exec pyupgrade --py37-plus {} \;
    ```
    
    It could be useful to add this weapon in tox test suite for instance.
    
    [^1]: https://github.com/asottile/pyupgrade

--- tests/test_fastapi_hypermodel.py
+++ tests/test_fastapi_hypermodel.py
@@ -23,7 +23,7 @@ def test_tpl(template):
 
 
 def test_get_value_for_key():
-    class Object(object):
+    class Object:
         pass
 
     o = Object()
@@ -39,7 +39,7 @@ def test_get_value_for_key_dict():
 
 
 def test_get_value_for_keys():
-    class Object(object):
+    class Object:
         pass
 
     o = Object()
@@ -54,7 +54,7 @@ def test_get_value_for_keys():
 
 
 def test_get_value():
-    class Object(object):
+    class Object:
         pass
 
     o = Object()
