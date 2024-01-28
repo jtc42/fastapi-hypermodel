c24a73a6ccc945b40ef2f553edcd49b0a5e47edf
Ezequiel Leonardo Castaño <14986783+ELC@users.noreply.github.com>
2024-01-27 21:57:46 -0300

    Sort imports in __all__

--- examples/hal/__init__.py
+++ examples/hal/__init__.py
@@ -1,4 +1,4 @@
 from examples.hal.app import Item, ItemSummary, Person, app
 from examples.hal.data import curies, items, people
 
-__all__ = ['Item', 'ItemSummary', 'Person', 'app', 'curies', 'items', 'people']
+__all__ = ["Item", "ItemSummary", "Person", "app", "curies", "items", "people"]
--- examples/siren/__init__.py
+++ examples/siren/__init__.py
@@ -1,4 +1,4 @@
 from examples.siren.app import Item, ItemSummary, Person, app
 from examples.siren.data import items, people
 
-__all__ = ['Item', 'ItemSummary', 'Person', 'app', 'items', 'people']
+__all__ = ["Item", "ItemSummary", "Person", "app", "items", "people"]
