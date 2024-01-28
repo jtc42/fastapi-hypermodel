c6cdc83e94e1681662e149ef9960561281f224ae
Ezequiel Leonardo Castaño <14986783+ELC@users.noreply.github.com>
2024-01-27 21:53:04 -0300

    Fix RUF022

--- examples/hal/__init__.py
+++ examples/hal/__init__.py
@@ -1,4 +1,4 @@
 from examples.hal.app import Item, ItemSummary, Person, app
 from examples.hal.data import curies, items, people
 
-__all__ = ["ItemSummary", "Item", "Person", "app", "items", "people", "curies"]
+__all__ = ['Item', 'ItemSummary', 'Person', 'app', 'curies', 'items', 'people']
